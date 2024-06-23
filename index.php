1c5b628a8e59a892518e3bb92c56ca99ed32aa15
Jeylani Osman <jey.osman@gmail.com>
2024-06-23 11:47:50 -0400

    Updated to v6

--- index.php
+++ index.php
@@ -7,3 +7,12 @@ define("PROOT", __DIR__ );
 define("DS", DIRECTORY_SEPARATOR);
 // var_dump(PROOT);
 var_dump(DS);
+
+// require_once('core/Config.php');
+// use Core\Config;
+
+// echo Config::get('db_name');
+// echo Config::get('db_user');
+
+
+
